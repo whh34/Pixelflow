d0656b3f3e8ccb2c37989e613deef6c998c0800b
whh34 <whh34@case.edu>
2017-03-30 21:58:03 -0400

    Committing a Test report generator

--- runtests.bat
+++ runtests.bat
@@ -1,4 +1,6 @@
 packages\OpenCover.4.6.519\tools\OpenCover.Console.exe -target:testrunner.bat -register:user -filter:+[PixelFlow]*
 packages\ReportGenerator.2.5.6\tools\reportgenerator.exe -reports:results.xml -targetdir:CoverageResults
+packages\ReportUnit.1.2.1\tools\ReportUnit.exe TestResult.xml TestResults.html
 move /y results.xml .\CoverageResults
 move /y TestResult.xml .\TestResults
+move /y TestResults.html .\TestResults
