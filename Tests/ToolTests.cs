877ab0fc9bf2e76cdcb901b1466b2e38d45b1880
Drake Poling <dsp45@case.edu>
2017-03-30 22:01:42 -0400

    Added ToolTests class

---
+++ Tests/ToolTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using PixelFlow;
+using static NUnit.Framework.Assert;
+
+namespace Tests
+{
+    [TestFixture]
+    class ToolTests
+    {
+
+        [Test]
+        public void PencilTest()
+        {
+
+        }
+
+        [Test]
+        public void LineTest()
+        {
+
+        }
+    }
+}
